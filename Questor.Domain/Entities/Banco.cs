b831ce8aa605634723163454ddc63e648516ce22
Fabio <fabioaugustodametto@gmail.com>
2024-11-20 13:33:15 -0300

    Entidades de dominio

---
+++ Questor.Domain/Entities/Banco.cs
@@ -0,0 +1,16 @@
+﻿using Questor.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questor.Domain.Entities
+{
+    public class Banco : BaseModel
+    {
+        public string Nome {  get; set; }
+        public string Codigo { get; set; }
+        public string Juros { get; set; }
+    }
+}
---
+++ Questor.Domain/Entities/Boleto.cs
@@ -0,0 +1,21 @@
+﻿using Questor.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questor.Domain.Entities
+{
+    public class Boleto : BaseModel
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string NomeBeneficiario { get; set; }
+        public string CPFBeneficiario { get; set; }
+        public double Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public int IdBanco { get; set; }
+        public Banco Banco { get; set; }
+    }
+}
